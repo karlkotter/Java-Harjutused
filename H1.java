ea74bf220d5105d9ee7dd0e3e03cf24f331e69c8
McKotter <93869121+mckotter@users.noreply.github.com>
2022-09-05 22:24:55 +0300

    Add files via upload

---
+++ H1.java
@@ -0,0 +1,39 @@
+public class H1{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//Muutuja nimega "nimi", mis hoiab minu nime
+		String nimi = new String();
+		nimi = "Karl";
+
+		//Muutuja nimega "kursus", mis hoiustab kursuse t2hiseid, kus ma 6pin
+		String kursus = new String();
+		kursus = "IT-21";
+
+		//V2ljastab terminali muutuja "nimi" koos muutuja "kursus"-ega
+		System.out.println(nimi+" "+kursus);
+
+		//Muutuja nimega "vanus"
+		int vanus = 16;
+
+		//Muutuja nimega "pikkus", mis lubab panna komaarve
+		double pikkus = 1.93;
+
+		//Muutuja nimega "hnimi", hyydnime jaoks
+		String hnimi = "Karla";
+
+		//Lause, mis v2ljastatakse terminali
+		System.out.println("Minu nimi on "+nimi+", ma olen "+vanus+" aastat vana ja pikkuseks on "+pikkus+"m. Mind kutsutakse "+hnimi+"ks.");
+
+		//T2isarvuline muutuja
+		int minuArv = 
+
+		//V2ljastan muutuja "minuArv", kus puudub v22rtus
+		System.out.println(minuArv);
+
+		//Tulemuseks on "void cannot be converted to int"
+	}
+}
